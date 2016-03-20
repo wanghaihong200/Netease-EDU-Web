d472df709dbd5be432cef65740272b0a5baa030a
Loogeek <loogeek@163.com>
2016-03-20 18:32:28 +0800

    增加对pause方法的判断

--- js/main.js
+++ js/main.js
@@ -215,7 +215,9 @@
     });
     // 关闭视频弹出框
     MainUtil.addEvent(oClosePlay,'click',function(){
-      oVideoBtn.pause();                             // 关闭视频后视频暂停播放
+      if(oVideoBtn.pause){
+        oVideoBtn.pause();                             // 关闭视频后视频暂停播放
+      }
       oIntBig.style.display = "none";                // 隐藏视频播放弹出框
       oMask.className = '';                          // 隐藏遮罩层
     });
